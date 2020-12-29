c104babbc453f2e4baa36439b97d6303fc18f6da
amanina16 <amanina16@users.noreply.github.com>
2020-12-29 11:15:21 +0700

    first commit

---
+++ Lantai/Lantai.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Lantai.aspx.cs" Inherits="Lantai" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h4>LOKASI LOKER</h4>
+        <asp:Label ID="lbl" runat="server">Masukkan no Loker</asp:Label><br />
+        <asp:TextBox ID="txtloker" runat="server"></asp:TextBox>
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit"
+          onclick="btnSubmit_Click" />
+    
+    </div>
+    </form>
+
+    <div id="dvlokasi" runat="server">
+        <h4> <asp:Label ID="lbllokasi" runat="server"></asp:Label><br /></h4>
+    </div>
+</body>
+</html>
---
+++ Lantai/Lantai.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Lantai : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        dvlokasi.Visible = false;
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        int lantai = 0;
+        int noloker = Convert.ToInt32(txtloker.Text);
+        int nomer = 0;
+        txtloker.Text = "";
+
+        if (noloker == 0)
+        {
+            lbllokasi.Text = "Tidak ada loker nomer 0";
+        }
+        else
+        {
+            while (noloker > nomer)
+            {
+                lantai++;
+                nomer = nomer + 9;
+                if (noloker <= nomer)
+                {
+                    break;
+                }
+                lantai++;
+                nomer = nomer + 3;
+                if (noloker <= nomer)
+                {
+                    break;
+                }
+                lantai++;
+                nomer = nomer + 7;
+                if (noloker <= nomer)
+                {
+                    break;
+                }
+                lantai++;
+                nomer = nomer + 2;
+                if (noloker <= nomer)
+                {
+                    break;
+                }
+
+            }
+
+            lbllokasi.Text = "LOKASI LOKER NO " + noloker + " BERADA DI LANTAI " + lantai;
+        }
+        dvlokasi.Visible = true;
+        txtloker.Text = "";
+    }
+}
